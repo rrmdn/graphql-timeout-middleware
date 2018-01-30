aad574c1d2c5107de2accdc9e21e8c3ba38109ca
Rizki Romadhoni <donnystark@salestock.id>
2018-01-31 05:26:35 +0700

    add build scripts

--- src/GraphQLTimeoutError.js
+++ src/GraphQLTimeoutError.js
@@ -2,6 +2,8 @@
 
 export default class GraphQLTimeoutError extends Error {
   constructor(field: string, waitingDurationInMs: number) {
-    super(`GraphQL Timeout Error: ${field} has timed out after waiting for ${waitingDurationInMs}ms`);
+    super(
+      `GraphQL Timeout Error: ${field} has timed out after waiting for ${waitingDurationInMs}ms`
+    );
   }
 }
